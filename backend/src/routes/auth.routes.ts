9d04df3e8bf4b04559e7ca92bfa3c7983a25811a
Valenmar03 <valenmrtnz8@gmail.com>
2026-03-18 12:23:54 -0300

    .

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,12 +4,13 @@ import {
   refreshHandler,
   logoutHandler,
   getBusinessBySlugHandler,
+  registerHandler
 } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.get("/business/:slug", getBusinessBySlugHandler);
-// router.post("/register", registerHandler); // disabled until public launch
+router.post("/register", registerHandler); // disabled until public launch
 router.post("/login", loginHandler);
 router.post("/refresh", refreshHandler);
 router.post("/logout", logoutHandler);
